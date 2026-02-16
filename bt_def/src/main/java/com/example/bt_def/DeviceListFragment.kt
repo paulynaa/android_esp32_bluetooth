a58a0e17dd353ce270e4b8405d11e80347b763b7
unknown <paulina.podgaiska@gmail.com>
2026-02-16 12:51:55 +0200

    bluetooth mygtukas

--- bt_def/src/main/java/com/example/bt_def/DeviceListFragment.kt
+++ bt_def/src/main/java/com/example/bt_def/DeviceListFragment.kt
@@ -1,19 +1,26 @@
 package com.example.bt_def
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.bt_def.databinding.FragmentListBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class DeviceListFragment : Fragment() {
     private var bAdapter: BluetoothAdapter? = null
     private lateinit var binding: FragmentListBinding
+
+    private lateinit var btLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +33,10 @@ class DeviceListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.imBluetoothOn.setOnClickListener {
+            btLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+        }
+        registerBtLauncher()
         initBtAdapter()
         bluetoothState()
     }
@@ -37,6 +48,18 @@ class DeviceListFragment : Fragment() {
     private fun bluetoothState() {
         if (bAdapter?.isEnabled == true){
             changeButtonColor(binding.imBluetoothOn, R.color.purple)
+        }
+    }
+    private fun registerBtLauncher(){
+        btLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                changeButtonColor(binding.imBluetoothOn, R.color.purple)
+                Snackbar.make(binding.root, "Bluetooth is turned on", Snackbar.LENGTH_LONG).show()
+
+            } else {
+                Snackbar.make(binding.root, "Bluetooth is turned off", Snackbar.LENGTH_LONG).show()
+            }
         }
     }
 }
